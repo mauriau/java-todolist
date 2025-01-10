a05a667ad25cc64720e2c72691e226b3d5cf5d62
mauriau <mauriau.tech@proton.me>
2025-01-10 18:13:52 +0100

    feat: init todo java project

---
+++ src/main/java/net/learntime/todo_api_1/TodoApi1Application.java
@@ -0,0 +1,13 @@
+package net.learntime.todo_api_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApi1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoApi1Application.class, args);
+    }
+
+}
---
+++ src/test/java/net/learntime/todo_api_1/TodoApi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.learntime.todo_api_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApi1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
